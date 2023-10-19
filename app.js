f852681e80ca5acd1f702ec39671abf17b804798
Luaranet <luaranet08@gmail.com>
2023-10-19 16:57:02 -0400

    commit

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const produtos = [ 
+  {id: 1, nome: "iron man", preco:"4.499,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 2, nome: "mulher maravilha", preco:"4.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 3, nome: "the flash", preco:"4.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 4, nome: "super girl", preco:"8.599,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 5, nome: "super man", preco:"5.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 6, nome: "arrow", preco:"2.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 7, nome: "power ranger", preco:"1.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 8, nome: "homem aranha", preco:"5,999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 9, nome: "loki", preco:"2.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+  {id: 10, nome: "feiticeira escarlate", preco:"5.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
+]
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 

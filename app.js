a3014aed9d496755a38f0832fd77bf6ebe540d1d
Luaranet <luaranet08@gmail.com>
2023-11-16 16:43:52 -0400

    comit

--- app.js
+++ app.js
@@ -15,11 +15,18 @@ const produtos = [
   {id: 10, nome: "feiticeira escarlate", preco:"5.999,00", descricao:"ndfkshf", imagem:"produto.jpg"},
 ]
 
+
+function buscarProdutoPorID(id){
+  const produto = produtos.find(produto => produto.id = id);
+  return produto || null 
+  }
+
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { produtos });
 });
 
 app.get('/produtos', (req, res) => {
